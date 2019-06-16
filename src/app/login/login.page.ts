d8767768cc3540b34624c22d982d72401605eb06
ayush8010720467 <ayushkatiyar096@gmail.com>
2019-06-16 22:58:07 +0530

    Login for the eisting user

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  email: string;
+  password: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+  doLogin() {
+    firebase.auth().signInWithEmailAndPassword(this.email, this.password).then((data) => {
+      console.log(data);
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+
+}
